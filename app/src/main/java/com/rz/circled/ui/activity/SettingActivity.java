18b57efc43e86242f73da3a13679299be27af94c
huying <huying@yryz.com>
2017-09-25 20:23:55 +0800

    qh832  918

--- app/src/main/java/com/rz/circled/ui/activity/SettingActivity.java
+++ app/src/main/java/com/rz/circled/ui/activity/SettingActivity.java
@@ -200,7 +200,6 @@ public class SettingActivity extends BaseActivity {
                         Session.clearShareP();
                         Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
                         startActivity(intent);
-                        finish();
                         EventBus.getDefault().post(new BaseEvent(CommonCode.EventType.TYPE_LOGOUT));
 
                     }
