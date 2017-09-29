52b8b8fea1d95b328af798882ae92a0b331ce15d
gsm <date0107@163.com>
2017-09-29 14:35:48 +0800

    合并代码

--- app/src/main/java/com/rz/circled/adapter/RewardAdapter.java
+++ app/src/main/java/com/rz/circled/adapter/RewardAdapter.java
@@ -3,6 +3,7 @@ package com.rz.circled.adapter;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,6 +17,7 @@ import com.rz.common.adapter.CommonAdapter;
 import com.rz.common.adapter.ViewHolder;
 import com.rz.common.utils.TimeUtil;
 import com.rz.httpapi.bean.MyRewardBean;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -48,32 +50,32 @@ public class RewardAdapter extends CommonAdapter {
         ImageView picImg = (ImageView) helper.getViewById(R.id.pic_logo_img);
 
 
-        if(!TextUtils.isEmpty(model.getUser().getCustNname())){
+        if (!TextUtils.isEmpty(model.getUser().getCustNname())) {
             tvName.setText(model.getUser().getCustNname());
         }
 
-        if(!TextUtils.isEmpty(model.getUser().getCustImg())){
+        if (!TextUtils.isEmpty(model.getUser().getCustImg())) {
             Glide.with(mContext).load(model.getUser().getCustImg()).transform(new GlideCircleImage(mContext)).into(avatarImg);
         }
 
-        if(!TextUtils.isEmpty(model.getPics())){
-            rewardTxt.setText("悬赏金额 "+model.getPrice());
+        if (!TextUtils.isEmpty(model.getPics())) {
+            rewardTxt.setText("悬赏金额 " + model.getPrice());
         }
 
-        if(!TextUtils.isEmpty(model.getContent())){
+        if (!TextUtils.isEmpty(model.getContent())) {
             tvContent.setText(model.getContent());
         }
 
-        if(!TextUtils.isEmpty(model.getLocation())){
+        if (!TextUtils.isEmpty(model.getLocation())) {
             placeLayout.setVisibility(View.VISIBLE);
             tvPlace.setText(model.getLocation());
-        }else{
+        } else {
             placeLayout.setVisibility(View.GONE);
         }
 
-        if( TextUtils.isEmpty(model.getPics())){
+        if (TextUtils.isEmpty(model.getPics())) {
             picImg.setVisibility(View.GONE);
-        }else{
+        } else {
             picImg.setVisibility(View.VISIBLE);
         }
 
@@ -84,42 +86,42 @@ public class RewardAdapter extends CommonAdapter {
         String dateNowStr = sdf.format(d);
 
         //判断悬赏是否在进行中
-        if(model.getComplete() == 1){
-            tvStatus.setTextColor(mContext.getColor(R.color.colorAccent));
+        if (model.getComplete() == 1) {
+            tvStatus.setTextColor(ContextCompat.getColor(mContext, R.color.colorAccent));
             tvStatus.setText("进行中");
             tvTime.setVisibility(View.VISIBLE);
 
-            String time = TimeUtil.getTime(dateNowStr,model.getTerminalTime());
-            String timeArray[] = time.replace("-","").split(",");
+            String time = TimeUtil.getTime(dateNowStr, model.getTerminalTime());
+            String timeArray[] = time.replace("-", "").split(",");
             String time1 = timeArray[0];
             String time2 = timeArray[1];
             String time3 = timeArray[2];
 
-            if(Integer.parseInt(time1)>0){
-                tvTime.setText("还剩" + time1+"天" + time2 + "时" + time3 + "分");
-            }else{
-                if(Integer.parseInt(time2)>0){
+            if (Integer.parseInt(time1) > 0) {
+                tvTime.setText("还剩" + time1 + "天" + time2 + "时" + time3 + "分");
+            } else {
+                if (Integer.parseInt(time2) > 0) {
                     tvTime.setText("还剩" + time2 + "时" + time3 + "分");
-                }else{
+                } else {
                     tvTime.setText("还剩" + time3 + "分");
                 }
             }
 //            tvTime.setText("还剩"+TimeUtil.getTime(dateNowStr,model.getTerminalTime()));
-        }else if(model.getComplete() == 2){
-            tvStatus.setTextColor(mContext.getColor(R.color.font_gray_m));
+        } else if (model.getComplete() == 2) {
+            tvStatus.setTextColor(ContextCompat.getColor(mContext, R.color.font_gray_m));
             tvStatus.setText("已完成");
             tvTime.setVisibility(View.GONE);
-        }else if(model.getComplete() == 3){
-            tvStatus.setTextColor(mContext.getColor(R.color.font_gray_m));
+        } else if (model.getComplete() == 3) {
+            tvStatus.setTextColor(ContextCompat.getColor(mContext, R.color.font_gray_m));
             tvTime.setVisibility(View.GONE);
             tvStatus.setText("已过期");
         }
 
-        if(model.getReplyNum() == 0){
+        if (model.getReplyNum() == 0) {
             tvPerNum.setVisibility(View.GONE);
-        }else{
+        } else {
             tvPerNum.setVisibility(View.VISIBLE);
-            tvPerNum.setText(model.getReplyNum()+"人参加");
+            tvPerNum.setText(model.getReplyNum() + "人参加");
         }
     }
 
