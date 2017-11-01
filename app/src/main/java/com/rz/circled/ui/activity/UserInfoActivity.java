50563abe9b17898295cc5f0114f84ed0106fe861
huying <huying@yryz.com>
2017-11-01 17:38:31 +0800

    qh2000

--- app/src/main/java/com/rz/circled/ui/activity/UserInfoActivity.java
+++ app/src/main/java/com/rz/circled/ui/activity/UserInfoActivity.java
@@ -188,7 +188,7 @@ public class UserInfoActivity extends BaseActivity {
     public void initHead() {
 //        avatarLayout.getBackground().setAlpha(66);
         newTitilbar = View.inflate(this, R.layout.titlebar_user_info, null);
-        newTitilbar.setBackgroundResource(R.mipmap.topbar_blue_top);
+//        newTitilbar.setBackgroundResource(R.mipmap.topbar_blue_top);
         newTitilbar.getBackground().mutate().setAlpha(255);
         TextView tv = (TextView) newTitilbar.findViewById(R.id.titlebar_main_tv);
         ImageView ib = (ImageView) newTitilbar.findViewById(R.id.titlebar_main_left_btn);
