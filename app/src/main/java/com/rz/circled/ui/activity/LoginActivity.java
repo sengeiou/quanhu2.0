6a1171b9bbf73ec75e2cb0811397d65c70c24493
[lixiang] <[282650760@qq.com]>
2017-11-07 18:30:43 +0800

    Merge branch 'v1.0.5' of http://192.168.30.4/quanhu/android-quanhu into v1.0.5
    # Conflicts:
    #	app/src/main/java/com/rz/circled/ui/activity/LoginActivity.java

--- app/src/main/java/com/rz/circled/ui/activity/LoginActivity.java
+++ app/src/main/java/com/rz/circled/ui/activity/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.rz.circled.ui.activity;
 
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
