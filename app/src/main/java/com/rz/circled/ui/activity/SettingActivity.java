94801a3cd1f9198e1520813db7a357a00c985bd5
huying <huying@yryz.com>
2017-11-08 09:52:17 +0800

    登出修改

--- app/src/main/java/com/rz/circled/ui/activity/SettingActivity.java
+++ app/src/main/java/com/rz/circled/ui/activity/SettingActivity.java
@@ -236,14 +236,14 @@ public class SettingActivity extends BaseActivity {
                         }
                         dialog.dismiss();
                         presenter.ExitApp();
-//                        exitApp();
+                        exitApp();
                         Session.clearShareP();
                         Intent intent = new Intent(SettingActivity.this, MainActivity.class);
-//                        intent.putExtra(IntentKey.EXTRA_TYPE, CommonCode.Constant.TAB_MAIN_HOME);
+                        intent.putExtra(IntentKey.EXTRA_TYPE, CommonCode.Constant.TAB_MAIN_HOME);
                         startActivity(intent);
 
-//                        EventBus.getDefault().post(new BaseEvent(CommonCode.EventType.TYPE_LOGOUT));
-
+                        EventBus.getDefault().post(new BaseEvent(CommonCode.EventType.TYPE_LOGOUT));
+                        finish();
                     }
                 });
                 dialogView.findViewById(R.id.id_tv_cancel).setOnClickListener(new View.OnClickListener() {
@@ -287,7 +287,7 @@ public class SettingActivity extends BaseActivity {
 
 //        MobclickAgent.onProfileSignOff();
 
-        setResult(IntentCode.Setting.SETTING_RESULT_CODE);
+//        setResult(IntentCode.Setting.SETTING_RESULT_CODE);
 
         finish();
     }
